391dfe153f70e04ab878ded0bbf40e5c07a6957c
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-02 15:10:12 +0200

    Add Canvas feature and controller classes

---
+++ server/NotedApi/Features/Canvases/Canvas.cs
@@ -0,0 +1,9 @@
+using NotedApi.Features.Notes;
+
+namespace NotedApi.Features.Canvases;
+
+public class Canvas
+{
+    public int Id { get; set; }
+    public List<Note> Notes { get; set; } = [];
+}
