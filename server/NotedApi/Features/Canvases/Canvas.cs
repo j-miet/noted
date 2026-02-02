0b593dec98912daf4ff8f38ea3612ac59794d28f
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-02 18:20:58 +0200

    Update feature models

--- server/NotedApi/Features/Canvases/Canvas.cs
+++ server/NotedApi/Features/Canvases/Canvas.cs
@@ -5,5 +5,7 @@ namespace NotedApi.Features.Canvases;
 public class Canvas
 {
     public int Id { get; set; }
+
+    public string Name { get; set; } = "";
     public List<Note> Notes { get; set; } = [];
 }
--- server/NotedApi/Features/Notes/Note.cs
+++ server/NotedApi/Features/Notes/Note.cs
@@ -1,9 +1,14 @@
+using NotedApi.Features.Canvases;
+
 namespace NotedApi.Features.Notes;
 
 public class Note
 {
     public int Id { get; set; }
+
     public int CanvasId { get; set; }
+    public Canvas Canvas { get; set; } = null!;
+
     public string Text { get; set; } = "";
     public int X { get; set; } = 300;
     public int Y { get; set; } = 200;
