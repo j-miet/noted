bec9df1519fb77285cc33f530d66f4d4e86176ce
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-03 16:33:24 +0200

    Add controller endpoint for canvases

--- server/NotedApi/Controllers/CanvasesController.cs
+++ server/NotedApi/Controllers/CanvasesController.cs
@@ -2,25 +2,66 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using NotedApi.Data;
+using NotedApi.Features.Canvases;
 
 namespace NotedApi.Controllers;
 
 [ApiController]
-[Route("api/[Controller]")]
-public class CanvasesController : ControllerBase
+[Route("api/canvas")]
+public class CanvasesController(NotedDbContext db) : ControllerBase
 {
-    private readonly NotedDbContext _db;
-
-    public CanvasesController(NotedDbContext db)
-    {
-        _db = db;
-    }
+    private readonly NotedDbContext _db = db;
 
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> GetAllCanvases()
     {
-        var canvases = await _db.Canvases.ToListAsync();
+        List<Canvas> canvases = await _db.Canvases.ToListAsync();
 
         return Ok(canvases);
+    }
+
+    [HttpGet("{canvasId}")]
+    public async Task<IActionResult> GetCanvasById(int canvasId)
+    {
+        Canvas? canvas = await _db.Canvases.Where(c => c.Id == canvasId).FirstOrDefaultAsync();
+
+        if (canvas == null) return NotFound();
+
+        return Ok(canvas);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCanvas(Canvas canvas)
+    {
+        _db.Canvases.Add(canvas);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCanvasById), new { id = canvas.Id }, canvas);
+    }
+
+    [HttpPatch("{canvasId}/name")]
+    public async Task<IActionResult> UpdateCanvasName(int canvasId, Canvas canvas)
+    {
+        Canvas? updateCanvas = await _db.Canvases.FindAsync(canvasId);
+
+        if (updateCanvas == null) return NotFound();
+
+        updateCanvas.Name = canvas.Name;
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{canvasId}")]
+    public async Task<IActionResult> DeleteCanvas(int canvasId)
+    {
+        Canvas? deletedCanvas = await _db.Canvases.FindAsync(canvasId);
+
+        if (deletedCanvas == null) return NotFound();
+
+        _db.Canvases.Remove(deletedCanvas);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
     }
 }
