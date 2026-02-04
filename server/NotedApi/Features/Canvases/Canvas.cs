c3e5b6186cb469a69317b02d456621e1c5da5b73
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-04 16:00:00 +0200

    Add doc

--- server/NotedApi/Features/Canvases/Canvas.cs
+++ server/NotedApi/Features/Canvases/Canvas.cs
@@ -2,10 +2,12 @@ using NotedApi.Features.Notes;
 
 namespace NotedApi.Features.Canvases;
 
+/// <summary>
+/// Canvas model
+/// </summary>
 public class Canvas
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = "";
     public List<Note> Notes { get; set; } = [];
 }
--- server/NotedApi/Features/Notes/Note.cs
+++ server/NotedApi/Features/Notes/Note.cs
@@ -2,13 +2,14 @@ using NotedApi.Features.Canvases;
 
 namespace NotedApi.Features.Notes;
 
+/// <summary>
+/// Note model
+/// </summary>
 public class Note
 {
     public int Id { get; set; }
-
     public int CanvasId { get; set; }
     public Canvas Canvas { get; set; } = null!;
-
     public string Text { get; set; } = "";
     public int X { get; set; } = 300;
     public int Y { get; set; } = 200;
