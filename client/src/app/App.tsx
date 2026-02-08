caa147776d3f6715b8d003596fc94efe81f6a218
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-08 20:34:17 +0200

    Add app base files

---
+++ client/src/app/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import CanvasPage from "./pages/CanvasList";
+
+export default function App() {
+  return (
+    <div className="noted-app">
+      <CanvasPage></CanvasPage>
+    </div>
+  );
+}
