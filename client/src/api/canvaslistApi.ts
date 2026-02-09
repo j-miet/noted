de29876d4c1220ac7db4f61e649d394e54d59ac3
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-09 17:17:40 +0200

    Add api for canvas list page

---
+++ client/src/api/canvaslistApi.ts
@@ -0,0 +1,43 @@
+import type { Canvas } from "../types/pages";
+import { apiRequest } from "./clientApi";
+
+/**
+ * API for canvas listing page's requests
+ */
+export const canvaslistApi = {
+  GetAllCanvases() {
+    return apiRequest("/api/canvas", {
+      method: "GET",
+    });
+  },
+
+  GetCanvasById(canvasId: number) {
+    return apiRequest(`/api/canvas/${canvasId}`, {
+      method: "POST",
+    });
+  },
+
+  CreateCanvas(name: string) {
+    return apiRequest(`/api/canvas`, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+      }),
+    });
+  },
+
+  UpdateCanvasName(canvas: Canvas) {
+    return apiRequest(`/api/canvas/${canvas.id}/name`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        name: canvas.name,
+      }),
+    });
+  },
+
+  DeleteCanvas(canvasId: number) {
+    return apiRequest(`/api/canvas/${canvasId}`, {
+      method: "DELETE",
+    });
+  },
+};
