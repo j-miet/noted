62d7509d028205dd8a9008ada72756824af0448b
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-09 17:18:50 +0200

    Update list page logic and styling

--- client/src/app/pages/CanvasList.tsx
+++ client/src/app/pages/CanvasList.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
-import { apiRequest } from "../requestApi";
 
 import "./CanvasList.css";
 import type { Canvas } from "../../types/pages";
+import { canvaslistApi } from "../../api/canvaslistApi.ts";
+import { nameCheck } from "../../utils/inputCheck.ts";
 
 export default function CanvasPage() {
   const [canvases, setCanvases] = useState<Canvas[]>([]);
 
-  const getCanvases = async () => {
+  const getAllCanvases = async () => {
     try {
-      const data = await apiRequest("/api/canvas");
+      const data = await canvaslistApi.GetAllCanvases();
 
       setCanvases(data);
     } catch (error) {
@@ -17,14 +18,55 @@ export default function CanvasPage() {
     }
   };
 
+  const createCanvas = async () => {
+    try {
+      const name = nameCheck(prompt("Name your canvas: "));
+      if (name === "") return;
+
+      await canvaslistApi.CreateCanvas(name);
+
+      getAllCanvases();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateCanvasName = async (canvas: Canvas) => {
+    try {
+      const name = nameCheck(prompt(`Rename canvas '${canvas.name}':`));
+      if (name === "") return;
+
+      canvas.name = name;
+      await canvaslistApi.UpdateCanvasName(canvas);
+
+      getAllCanvases();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteCanvas = async (canvas: Canvas) => {
+    try {
+      if (!confirm(`Delete canvas '${canvas.name}'?`)) return;
+
+      await canvaslistApi.DeleteCanvas(canvas.id);
+
+      getAllCanvases();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // load all canvases when page is refreshed, but not during rerenders
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    getCanvases();
+    getAllCanvases();
   }, []);
 
   return (
     <div className="canvaslist-page">
       <h3>My canvases</h3>
+      <hr></hr>
       <ul className="canvaslist-list">
         {canvases.map((canvas) => {
           if (!canvas) return null;
@@ -32,10 +74,26 @@ export default function CanvasPage() {
           return (
             <li key={canvas.id} className="canvaslist-item">
               {canvas.name}
+
+              <button
+                onClick={() => updateCanvasName(canvas)}
+                className="canvaslist-updatename"
+              >
+                Rename
+              </button>
+              <button
+                onClick={() => deleteCanvas(canvas)}
+                className="canvaslist-deleteitem"
+              >
+                X
+              </button>
             </li>
           );
         })}
       </ul>
+      <button onClick={createCanvas} className="canvaslist-additem">
+        New Canvas
+      </button>
     </div>
   );
 }
