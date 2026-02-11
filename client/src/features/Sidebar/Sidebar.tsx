2b66ec694ec1d60708ab2c761bf249e9426a6932
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-11 18:52:06 +0200

    Add Sidebar

---
+++ client/src/features/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+// sidebar for canvas controls
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <button>Note</button>
+      <button>...</button>
+    </aside>
+  );
+}
